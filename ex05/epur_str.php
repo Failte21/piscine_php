60676043ca4274b5d6ffcf1b438143610659245c
Léo Simon <failte@openmailbox.org>
2017-04-12 10:52:10 +0200

    repo cleared

--- ex05/epur_str.php
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/php
-<?PHP
-	echo trim(preg_replace('/ +/', ' ', $argv[1]));
-	echo "\n";
-?>
--- load.sh
+++
@@ -1,3 +0,0 @@
-mkdir ~/http/piscine/d01/$1
-python ~/init.py d01 $1 $2
-chmod 755 ~/http/piscine/d01/$1/$2.php
